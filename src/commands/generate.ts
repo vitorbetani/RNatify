9854db00dd3a81ff6bc4c68f300e27c81ebac505
Vitor Bettani <vitor.bettani@gmail.com>
2022-11-19 23:55:43 -0300

    Add Atom component template

--- src/commands/generate.ts
+++ src/commands/generate.ts
@@ -1,4 +1,41 @@
-import { GluegunToolbox } from "gluegun";
+import { GluegunToolbox } from 'gluegun'
+
+const generateAtom = (name) => {
+  return name
+}
+const generateMolecule = (name) => {
+  return name
+}
+const generateOrganism = (name) => {
+  return name
+}
+
+const TemplateTypes = {
+  atom: 'atom',
+  molecule: 'molecule',
+  organism: 'organism',
+}
+
+const generateComponent = (type = typeof TemplateTypes, name = '') => {
+  if (!name) {
+    throw new Error('--name should be a valid string')
+  }
+
+  switch (type) {
+    case TemplateTypes.atom:
+      return generateAtom(name)
+    case TemplateTypes.molecule:
+      return generateMolecule(name)
+    case TemplateTypes.organism:
+      return generateOrganism(name)
+    default:
+      throw new Error(
+        `--type is not a valid type. Valid types are: ${Object.values(
+          TemplateTypes
+        ).join(', ')}`
+      )
+  }
+}
 
 module.exports = {
   name: 'generate',
@@ -7,16 +44,23 @@ module.exports = {
     const {
       parameters,
       template: { generate },
-      print: { error },
-    } = toolbox;
+      print: { error, success },
+    } = toolbox
+    try {
+      const { type, name } = parameters.options
 
-    const { type, name } = parameters.options;
-
-    if (type && name) {
-
-      return;
+      const path = generateComponent(type, name)
+      generate({
+        template: `${type}.ts.ejs`,
+        directory: `./src/templates`,
+        target: `./src/components/${type}/${name}.ts`,
+        props: {
+          name,
+        },
+      })
+      success(`Succesfully generated ${name} as type ${type}. ${path}`)
+    } catch (e) {
+      error(e.message)
     }
-    
-    error("Invalid arguments. Accepted are: --type | --name")
-   },
+  },
 }
